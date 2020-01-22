697f8076dc326542b1f00a17d76bf244ce4cf4c9
Roman Gushchin <guro@fb.com>
2020-01-22 15:09:42 -0800

    some more progress

--- mem_frag.py
+++ mem_frag.py
@@ -4,8 +4,90 @@ from drgn.helpers.linux import *
 from drgn.helpers.linux.mm import for_each_page, pfn_to_page
 
 
-PGHead = 1 << prog.constant('PG_head')
+PGLocked = 1 << prog.constant('PG_locked')
+PGReferenced = 1 << prog.constant('PG_referenced')
+PGUptodate = 1 << prog.constant('PG_uptodate')
+PGDirty = 1 << prog.constant('PG_dirty')
+PGLru = 1 << prog.constant('PG_lru')
+PGActive = 1 << prog.constant('PG_active')
+PGWorkingset = 1 << prog.constant('PG_workingset')
+PGWaiters = 1 << prog.constant('PG_waiters')
+PGError = 1 << prog.constant('PG_error')
 PGSlab = 1 << prog.constant('PG_slab')
+PGOwner_priv_1 = 1 << prog.constant('PG_owner_priv_1')
+PGArch_1 = 1 << prog.constant('PG_arch_1')
+PGReserved = 1 << prog.constant('PG_reserved')
+PGPrivate = 1 << prog.constant('PG_private')
+PGPrivate_2 = 1 << prog.constant('PG_private_2')
+PGWriteback = 1 << prog.constant('PG_writeback')
+PGHead = 1 << prog.constant('PG_head')
+PGMappedtodisk = 1 << prog.constant('PG_mappedtodisk')
+PGReclaim = 1 << prog.constant('PG_reclaim')
+PGSwapbacked = 1 << prog.constant('PG_swapbacked')
+PGUnevictable = 1 << prog.constant('PG_unevictable')
+PGMlocked = 1 << prog.constant('PG_mlocked')
+PGUncached = 1 << prog.constant('PG_uncached')
+PGHwpoison = 1 << prog.constant('PG_hwpoison')
+
+
+def page_flags(page):
+    flags = page.flags
+    ret = []
+
+    if flags & PGHead:
+        ret += ['head']
+    if flags & PGSlab:
+        ret += ['slab']
+    if flags & PGLocked:
+        ret += ['locked']
+    if flags & PGReferenced:
+        ret += ['referenced']
+    if flags & PGUptodate:
+        ret += ['uptodate']
+    if flags & PGDirty:
+        ret += ['dirty']
+    if flags & PGLru:
+        ret += ['lru']
+    if flags & PGActive:
+        ret += ['active']
+    if flags & PGWorkingset:
+        ret += ['workingset']
+    if flags & PGWaiters:
+        ret += ['waiters']
+    if flags & PGError:
+        ret += ['error']
+    if flags & PGSlab:
+        ret += ['slab']
+    if flags & PGOwner_priv_1:
+        ret += ['owner_priv_1']
+    if flags & PGArch_1:
+        ret += ['arch_1']
+    if flags & PGReserved:
+        ret += ['reserved']
+    if flags & PGPrivate:
+        ret += ['private']
+    if flags & PGPrivate_2:
+        ret += ['private_2']
+    if flags & PGWriteback:
+        ret += ['writeback']
+    if flags & PGHead:
+        ret += ['head']
+    if flags & PGMappedtodisk:
+        ret += ['mappedtodisk']
+    if flags & PGReclaim:
+        ret += ['reclaim']
+    if flags & PGSwapbacked:
+        ret += ['swapbacked']
+    if flags & PGUnevictable:
+        ret += ['unevictable']
+    if flags & PGMlocked:
+        ret += ['mlocked']
+    if flags & PGUncached:
+        ret += ['uncached']
+    if flags & PGHwpoison:
+        ret += ['hwpoison']
+
+    return ret
 
 
 def pages_to_GB(pages):
@@ -50,51 +132,68 @@ def compound_order(page):
         return 0
 
 
+def inspect_slab_page(page):
+    print('      %s' % page.slab_cache.name.string_().decode('ascii'))
+
+
 def inspect_page(page):
-    if PageBuddy(page):
+    if PageSlab(page):
+        inspect_slab_page(page)
         return True
 
-    if PageSlab(page):
-        
+    if PageBuddy(page):
+        return False
 
-    print(page)
     return False
+    # desc = page_flags(page)
+    # desc += ['mapping %d' % page.mapping]
+    # desc += ['type %d' % page.page_type]
+    # desc += ['order %d' % compound_order(page)]
+    # print(desc)
 
     
-def inspect_pfn_range(zone, start_pfn, max_pfn):
-    ret = False
-    page = cast("struct page *", prog["vmemmap_base"]) + start_pfn
-    desc = ""
+def inspect_pfn_range(zone, start_pfn, step):
+    huge = False
+    page = cast('struct page *', prog['vmemmap_base']) + start_pfn
+    desc = ''
+
+    if start_pfn + step > zone.zone_start_pfn + zone.present_pages:
+        return False
 
     try:
         order = compound_order(page)
-        ret = PageHeadHuge(page)
+        huge = PageHeadHuge(page)
 
-        if ret:
+        if huge:
             if order == 18:
-                desc = "1GB hugepage"
+                desc = '1GB hugepage'
             elif order == 9:
-                desc = "2MB hugepage"
+                desc = '2MB hugepage'
             else:
-                desc = "unknown?? hugepage"
-        else:
-            for pfn in range(start_pfn, max_pfn):
-                page = cast("struct page *", prog["vmemmap_base"]) + pfn
-                if not inspect_page(page):
+                desc = 'unknown?? hugepage'
+
+        print('    %-10d (%s)' % (start_pfn, desc))
+
+        if not huge:
+            n = 0
+            for pfn in range(start_pfn, start_pfn + step):
+                page = cast('struct page *', prog['vmemmap_base']) + pfn
+                if inspect_page(page):
+                    n += 1
+                if n > 10:
                     break
 
     except drgn.FaultError as e:
-        desc = "error: %s" % e
+        print('error: %s' % e)
 
-    print("    %-10d (%s)" % (start_pfn - zone.zone_start_pfn, desc))
-    return ret
+    return huge
 
 
 def main():
     # 1GB step
-    pfn_step = 1024 * 1024 * 1024 // 4096
+    step = 1024 * 1024 * 1024 // 4096
     # 2MB step
-    # pfn_step = 2 * 1024 * 1024 // 4096
+    #step = 2 * 1024 * 1024 // 4096
     found = 0
     total = -1
 
@@ -103,7 +202,7 @@ def main():
             total = prog['hstates'][i].nr_huge_pages
 
     for node in for_each_online_node():
-        print("Node %d" % node.node_id)
+        print('Node %d' % node.node_id)
 
         for zid in range(node.nr_zones):
             zone = node.node_zones[zid]
@@ -116,22 +215,25 @@ def main():
             present = int(zone.present_pages)
             free = int(zone.vm_stat[prog.constant('NR_FREE_PAGES')].counter.value_())
 
-            print("  Zone %s" % zone.name.string_().decode('ascii'))
-            # print("        spanned %10d (%.1f GB)" % (spanned, pages_to_GB(spanned)))
-            # print("        present %10d (%.1f GB)" % (present, pages_to_GB(present)))
-            # print("        managed %10d (%.1f GB)" % (managed, pages_to_GB(managed)))
+            print('  Zone %s' % zone.name.string_().decode('ascii'))
+            # print('        spanned %10d (%.1f GB)' % (spanned, pages_to_GB(spanned)))
+            # print('        present %10d (%.1f GB)' % (present, pages_to_GB(present)))
+            # print('        managed %10d (%.1f GB)' % (managed, pages_to_GB(managed)))
 
-            # print("        free    %10d (%.1f Gb, %.1f%%)" %
+            # print('        free    %10d (%.1f Gb, %.1f%%)' %
             #       (free, pages_to_GB(free), free * 100 / managed))
+
+            start = zone.zone_start_pfn
+            if start % step:
+                start = start + step - start % step
             
-            for pfn in range(zone.zone_start_pfn, zone.zone_start_pfn + zone.present_pages,
-                             pfn_step):
-                if inspect_pfn_range(zone, pfn, min(pfn + pfn_step,
-                                                    zone.zone_start_pfn + zone.present_pages)):
+            for pfn in range(start, zone.zone_start_pfn + zone.present_pages,
+                             step):
+                if inspect_pfn_range(zone, pfn, step):
                     found += 1
 
-    print("--")
-    print("found %d of %d" % (found, total))
+    print('--')
+    print('found %d of %d' % (found, total))
 
 
 main()
