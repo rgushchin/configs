fccc6f45fc3a6fdc5c99d1367d62db3d7df2f02a
Roman Gushchin <guro@fb.com>
2020-02-12 14:09:32 -0800

    kvm.sh update

--- kvm.sh
+++ kvm.sh
@@ -1,7 +1,7 @@
 #/bin/sh
 
 KERNEL=arch/x86/boot/bzImage
-ROOTFS="/dev/vda2"
+ROOTFS="/dev/vda3 rootflags=subvol=root"
 CGROUPV2="cgroup_no_v1=all systemd.unified_cgroup_hierarchy=1"
 
 qemu-system-x86_64 \
@@ -12,7 +12,7 @@ qemu-system-x86_64 \
     -no-reboot \
     -smp 2 \
     -device e1000,netdev=user.0 -netdev user,id=user.0,hostfwd=tcp::10022-:22 \
-    -kernel ${KERNEL} \
+    -kernel ${KERNEL} ${1} \
     -append "root=${ROOTFS} ${CGROUPV2} console=ttyS0,115200" \
     -nographic
 
